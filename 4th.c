075a17cec8b757e0301d9b952cf30938692de305
Harish A <133196031+harishisinnocent@users.noreply.github.com>
2024-12-02 13:27:54 +0530

    Create 4th.c

---
+++ 4th.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void compressString(char *str) {
+    int i = 0, count;
+    while (str[i]) {
+        count = 1;
+        while (str[i] == str[i + 1]) { count++; i++; }
+        printf("%c", str[i]);
+        if (count > 1) printf("%d", count);
+        i++;
+    }
+}
+
+int main() {
+    char str[100];
+    scanf("%s", str);
+    compressString(str);
+    return 0;
+}
